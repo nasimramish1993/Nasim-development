71007a172b6f51dbf0663b65ad66d2fcf1ad7e21
nasimramish1993 <nasimmohseni1993@gmail.com>
2024-07-01 15:14:04 -0400

    input username

--- class-5/class-5.py
+++ class-5/class-5.py
@@ -3,12 +3,17 @@ print('class five')
 
 user_name = 'nasimmuhsiny'
 password = 'nasim'
+path_key = 'ghazni'
 
 user_name1 = input('enter user name: ')
 user_password = input('enter password: ')
+key_words = input('where were you born: ')
 
-if (user_name == user_name1 and user_password == password ):
+if (user_name == user_name1 and user_password == password and key_words == path_key ):
     print('login is successfully')
+
+elif (user_name != user_name1 and user_password == password and key_words == path_key ):
+    print('enter key words')
 
 else:
     print('you lost your account')
