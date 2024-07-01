28d07e9d6df821f9f5995317f763254bc60d52f7
nasimramish1993 <nasimmohseni1993@gmail.com>
2024-07-01 12:57:19 -0400

    tempreture

--- class-1/class-1.py
+++ class-1/class-1.py
@@ -1,57 +1,123 @@
-print('welcome to pyton')
+# print('welcome to pyton')
 
-# add the numiral 
-x = 23
-y = 5
-sum = x+y
-print(sum)
+# # add the numiral 
+# x = 23
+# y = 5
+# sum = x+y
+# print(sum)
 
 
-print ('Helloworld.py')
-# Add 10 years
-year = 2024
-year += 10
-print(year)
-
+# print ('Helloworld.py')
+# # Add 10 years
+# year = 2024
+# year += 10
 # print(year)
+
+# # print(year)
  
-# substruct 20
-# num_tow # type: ignore
+# # substruct 20
+# # num_tow # type: ignore
 
 
-area = 1/2*20*15
-base = 20
-height=15
+# area = 1/2*20*15
+# base = 20
+# height=15
 
-print(1/2,base*height)
-print(type(base))
+# print(1/2,base*height)
+# print(type(base))
 
-area = 1/2
+# area = 1/2
 
-base = 20
+# base = 20
 
-height = 15
+# height = 15
 
-# progrom to find the x base x height of a traingle
+# # progrom to find the x base x height of a traingle
 
-perimeter = 1/2 * (base * height)
+# perimeter = 1/2 * (base * height)
 
-print(perimeter) # output: 150
+# print(perimeter) # output: 150
 
-print(perimeter, height, base) # output: 1/2 15 20
+# print(perimeter, height, base) # output: 1/2 15 20
 
-print('the area of my tringle is', area) # type: ignore , # output: the area of my tringle is 150
+# print('the area of my tringle is', area) # type: ignore , # output: the area of my tringle is 150
 
-age1 = 25
-age =+ 5
-count_age = age1+age
-print(count_age)  
+# age1 = 25
+# age =+ 5
+# count_age = age1+age
+# print(count_age)  
 
-# fload 
-num_1 = 5.13
-print(type(num_1))
+# # fload 
+# num_1 = 5.13
+# print(type(num_1))
 
-# Boolean 
+# # Boolean 
 
-technical_error = True 
-print(type(technical_error))
+# technical_error = True 
+# print(type(technical_error))
+
+# demographic_ifo = {
+#   'first_name': 'nasim', 
+#   'last_name': 'muhsiny', 
+#   'state': 'NY'
+# }
+
+# print(demographic_ifo)
+
+# # casting 
+
+# my_string = '7'
+# print(my_string)
+# print(type(my_string))
+
+# my_int = int(my_string)
+# print(my_int)
+# print(type(my_int))
+
+# prints every character in the string one by one
+
+# for o in 'one_color':
+#     print(o)
+
+
+    # temp_f = 40
+    # temp_c = (temp_f-32)* 5/9
+    # print(temp_c)
+
+# statement (and or >, < , ==, =<, =>, !=)
+
+# x = 20
+# y = 30 
+# if x > y:
+#     print(x)
+
+# elif y > x:
+#     print(y)
+
+# x = 12
+# y = 23
+#  if x > y:
+#    print('x is bigger than y')
+#  elif y > x:
+#    print('y is bigeer than x')
+#  else:
+# print('they are the same')
+
+
+temp_f =87
+
+if temp_f > 75:
+  print('outside is too hot')
+elif temp_f > 40:
+  print('outside is normal')
+else:
+  print('outside is cold')
+
+  x = 23
+  y = 44
+  if x > y:
+    print('x bigeer than y')
+  elif y > x:
+    print('y bigeer than x')
+  else:
+    print('they are the same')
