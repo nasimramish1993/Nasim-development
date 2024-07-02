30da745ba5ee124ff278ccc02e2269ebd71ebe55
nasimramish1993 <nasimmohseni1993@gmail.com>
2024-07-02 18:24:33 -0400

    indexing

--- class-5/class-5.py
+++ class-5/class-5.py
@@ -1,20 +1,95 @@
-print('class five')
+# print('class five')
 
 
-user_name = 'nasimmuhsiny'
-password = 'nasim'
-path_key = 'ghazni'
+# user_name = 'nasimmuhsiny'
+# password = 'nasim'
+# path_key = 'ghazni'
 
-user_name1 = input('enter user name: ')
-user_password = input('enter password: ')
-key_words = input('where were you born: ')
+# user_name1 = input('enter user name: ')
+# user_password = input('enter password: ')
+# key_words = input('where were you born: ')
 
-if (user_name == user_name1 and user_password == password and key_words == path_key ):
-    print('login is successfully')
+# if (user_name == user_name1 and user_password == password and key_words == path_key ):
+#     print('login is successfully')
 
-elif (user_name != user_name1 and user_password == password and key_words == path_key ):
-    print('enter key words')
+# elif (user_name != user_name1 and user_password == password and key_words == path_key ):
+#     print('enter key words')
 
-else:
-    print('you lost your account')
+# else:
+#     print('you lost your account')
 
+
+
+
+# Get user input
+# user = input("Enter your input: ")
+# print(f'The user input is {user}')
+
+# # Figure out the length
+# user_length = len(user)
+# print(f"The length of the input is {user_length}\n")
+
+# # Create the border
+# border = '*' * (user_length + 2)
+
+# # Generate output
+# # Create the output with print() functions
+# print(border)
+# print('*' + user + '*')
+# print(border)
+
+
+# user = 'NASIM'
+# User_1 = user.lower()
+# print(User_1)
+
+# result = user.len()
+
+# indexing
+
+# word = 'nasim'
+# length = len(word)
+# print(length)
+# first_letter = word[0]
+# print(first_letter)
+# print(word[1])
+# print(word[2])
+# print(word[3])
+# print(word[4])
+
+name = 'nasim'
+e_letter = name.index('n')
+print(e_letter)
+
+name = 'abdulali'
+print(name[2])
+print(name[5])
+slicing = name[2:5]
+print(slicing)
+
+country = 'Amireca'
+print(country[4])
+print(country[6])
+slicing = country[4:6]
+print(slicing)
+
+
+user = input('Enter your name: ')
+print(f'the user input is {user}')
+
+# figure out the length
+user_length = len(user)
+print(f'this user has {user_length} digits')
+
+# creat the boarder
+
+# boarder = '*' * (user_length + 2)
+
+# print(boarder)
+# print('*' + user + '*')
+# print(boarder)
+
+out_line = '*' * 10
+print(out_line)
+print('*' + user + '*')
+print(out_line)
