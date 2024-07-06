fe1de472c30f77d660158fec978cbe1cd209a71d
nasimramish1993 <nasimmohseni1993@gmail.com>
2024-07-06 11:48:05 -0400

    is it number or alnumiroc

--- class-4/class-4.py
+++ class-4/class-4.py
@@ -4,12 +4,44 @@
 # if Bname == 'Abdulkhaliq':
 #     print('Hello my sweet brother')
 
-user_name = input('please enter the tempreture: ')
-user_name = int(user_name)
-if user_name > 60 :
-    print('it is too hot')
+# user_name = input('please enter the tempreture: ')
+# user_name = int(user_name)
+# if user_name > 60 :
+#     print('it is too hot')
 
-elif user_name  > 45 :
-    print('the weather is normal')
+# elif user_name  > 45 :
+#     print('the weather is normal')
+# else:
+#         print('it is too coold')
+
+# userin = input('Enter your numbers: ')
+# userin = int(userin)
+# if userin % 2 != 0:
+#     # print(f'{userin} is odd')
+
+# userin = input('Enter your numbers')
+# userin = int(userin)
+# print(f'{userin} is even')
+# if userin % 2 == 0:
+#     # print(userin)
+
+# userin = input('please enter your numbers: ')
+# userin = float(userin)
+
+# if userin % 2 == 1:
+#     print(f'{userin} is odd')
+# elif userin % 2 == 0:
+#     print(f'{userin} is even')
+# else:
+#     print('unknown')
+
+   # checking for somethings 
+userin = input('Enter your text: ')
+if userin.isdecimal():
+    print('this is number')
+elif userin.isalpha():
+    print('this is words')
+
 else:
-        print('it is too coold')
+    print(' somethig else')
+
