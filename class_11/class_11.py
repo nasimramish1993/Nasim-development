e9cfdaa4e54a949b9df9d37faa2b23aea90757e8
nasimramish1993 <nasimmohseni1993@gmail.com>
2024-07-15 19:23:24 -0400

    2d list

--- class_11/class_11.py
+++ class_11/class_11.py
@@ -1,21 +1,43 @@
 
 
-x = []
+# x = []
 
-original_list = [34, 57, 81, 92, 2, 13]
-even_list_of_num = [x for x in original_list if original_list %2 ==0]
-print(even_list_of_num)
+# original_list = [34, 57, 81, 92, 2, 13]
+# even_list_of_num = [x for x in original_list if original_list %2 ==0]
+# print(even_list_of_num)
 
 ''' Exercise:
 
 Write a 2D list that is a 3x3 grid of numbers
 Write some code that prints out that grid nicely wither proper formatting.
-'''
 
-lis = [[1,2,3], [4,5,6], [7,8,9]]
 
-for row in lis :
-    print(row)
-    for column in row :
-        print(column, end= ' ')
-        print('')
+
+# you are given a 2D list representing a table of data rows and columns .
+# write a script to calculate the sum and average of each column in the table.
+
+data = [[34, 54, 65],[78, 56, 45],[56, 45, 34]]
+counter = 1
+sum = 0
+for row in data :
+    for column in row:
+        print(column)
+        sum += column
+        ave =sum/3
+    print(f'counter: {counter} sum : {sum} ave: {ave} ')
+    counter += 1
+print('')
+
+''' creating a list via for loop vs list comprehension
+let's say you have a list of vegetable, and you want a new list containing only the vegetables that are less than 6 letters long.
+''' 
+# for loop
+
+vegetable = ['broccoli', 'kale', 'onion', 'garlic', 'kale']
+new_list_of_vegetable = []
+for v in vegetable:
+    if len(v) < 6:
+        new_list_of_vegetable.append(v)
+print(new_list_of_vegetable)
+  
+   
---
+++ class_12/class_12.py
@@ -0,0 +1,10 @@
+transportation = {'cars', 'bikes', 'plane'}
+
+new_transportation = transportation.clear()
+print(new_transportation)
+
+# Remove vermont from our set
+states = {'california', 'new york', 'vermont', 'connecticut'}
+
+states.remove('vermont')
+print(states)
